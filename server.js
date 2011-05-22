556c7df1ff7ffaf1d8312bded52df5dc66a8d840
Nick Payne <nick@kurai.co.uk>
2011-05-22 23:52:57 +0100

    got server displaying messages

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var http = require('http'),
     util = require('util'),
     zeromq  = require('zeromq');
 
-var queue = zeromq.createSocket('rep');
+var queue = zeromq.createSocket('pull');
 
-queue.bind('tcp://127.0.0.1:5554', function(err) {
+queue.bind('tcp://127.0.0.1:5556', function(err) {
     if (err) throw err;
+    util.debug('bound ZMQ pull server');
+
     queue.on('message', function(data) {
-        util.debug("got data "+data.toString("utf8"));
+        util.debug(data);
     });
 });
--- stream_processor.js
+++ stream_processor.js
@@ -2,9 +2,21 @@ var util = require('util'),
     zeromq  = require('zeromq');
 
 var queue = zeromq.createSocket('pull');
+queue.connect('tcp://127.0.0.1:5554');
 
+var push = zeromq.createSocket('push');
+push.connect('tcp://127.0.0.1:5556');
+
+util.debug("connected to pull & push servers");
+
+var repData = {};
 queue.on('message', function(data) {
-    util.debug("got data");
+    try {
+        data = JSON.parse(data);
+        repData.text = data.text;
+        push.send(JSON.stringify(repData));
+    } catch (e) {
+        util.debug(data);
+    }
 });
 
-queue.connect('tcp://127.0.0.1:5554');
