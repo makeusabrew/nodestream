6fa1e93ab39707fabb2f4fe3c92b54fac2fd8f4d
Nick Payne <nick@kurai.co.uk>
2011-05-25 18:10:15 +0100

    tinkering with server, adding some DB flavour

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-var http = require('http'),
-    util = require('util'),
+var http    = require('http'),
+    util    = require('util'),
     zeromq  = require('zeromq'),
-    io = require('socket.io');
+    express = require('express'),
+    io      = require('socket.io'),
+    mongo   = require('mongodb'),
+    redis   = require('redis');
+
+/**
+ * DB init
+ */
+db = new mongo.Db('nodestream', new mongo.Server('localhost', mongo.Connection.DEFAULT_PORT, {}), {});
+db.open(function(err, client) {
+    if (err) throw err;
+    // all good
+});
+
+redis = redis.createClient();
 
 /**
  * Queue goodies
@@ -13,19 +27,54 @@ queue.bind('tcp://127.0.0.1:5556', function(err) {
     util.debug('bound ZMQ pull server');
 
     queue.on('message', function(data) {
-        util.debug(data);
-        //socket.broadcast(data);
+        //util.debug(data);
+        var details = JSON.parse(data);
+        if (details.urls) {
+            /*
+            db.collection('tweets', function(err, collection) {
+                if (err) throw err;
+                collection.insert(details);
+            });
+            */
+            db.collection('urls', function(err, collection) {
+                if (err) throw err;
+                details.urls.forEach(function(url) {
+                    redis.incr(url, function(err, val) {
+                        util.debug(val);
+                        var data = {
+                            "url": url,
+                            "count": val
+                        };
+                        collection.update({url: url}, {$set: data}, {upsert:true});
+                    });
+                });
+            });
+        }
     });
 });
 
 /**
  * Server setup
  */
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end('todo');
+var app = express.createServer();
+
+app.configure(function() {
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/../public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.set('view engine', 'jade');
 });
 
-server.listen(8124);
+app.get('/', function(req, res) {
+    res.send("OK");
+});
 
-var socket = io.listen(server);
+
+app.listen(8124);
+
+/**
+ * Socket stuff
+ */
+var socket = io.listen(app);
